160445247c399e8a09a8f81e68e78e4f94ca1051
nakyeong <yunna3425@naver.com>
2025-10-01 03:55:15 +0900

    Feat: 페이지네이션을 통한 동적 영화 목록 구현

--- week3_movie/src/App.tsx
+++ week3_movie/src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css';
+import HomePage from './pages/HomePage';
 import MoviePage from "./pages/MoviePage";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFoundPage from './pages/NotFoundPage';
+import MovieDetailPage from './pages/MovieDetailPage';
 
-const App = () => {
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: 'movies/:category',
+        element: <MoviePage />,
+      },
+      {
+        path: 'movie/:movieId',
+        element: <MovieDetailPage />,
+      },
+    ]
+  },
+])
+
+function App() {
   return (
-    <>
-      <MoviePage />
-    </>
+    <RouterProvider router={router} />
   )
 }
 

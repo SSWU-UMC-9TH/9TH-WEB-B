ef867f0f2d1575caf413ace22d1346ecac20ae68
nakyeong <yunna3425@naver.com>
2025-11-09 05:23:21 +0900

    Feat: 무한스크롤 및 스켈레톤 UI

--- week6_lp/src/pages/HomePage.tsx
+++ week6_lp/src/pages/HomePage.tsx
@@ -1,25 +1,42 @@
-import React, { useState } from 'react'
-import useGetLpList from '../hooks/queries/useGetLpList'
+import React, { useEffect, useState } from 'react'
+import { useGetInfiniteLpList } from '../hooks/queries/useGetInfiniteLpList';
+import { PAGINATION_ORDER } from '../enums/common';
+import { useInView } from 'react-intersection-observer';
+import LpCard from '../components/LpCard/LpCard';
+import LpCardSkeletonList from '../components/LpCard/LpCardSkeletonList';
 
 const HomePage = () => {
     const [search, setSearch] = useState("");
-    const {data, isPending, isError} = useGetLpList({
-        search,
-    });
+    const {data: lps, isFetching, hasNextPage, isPending, fetchNextPage, isError} = useGetInfiniteLpList(10, search, PAGINATION_ORDER.desc);
 
+    // ref: 특정한 HTML 요소를 감시할 수 있다
+    // inView: 그 요소가 화면에 보이면 true
+    const {ref, inView} = useInView({
+        threshold: 0,
+    })
 
-    if (isPending) {
-        return <div>Loading...</div>
-    }
+    useEffect(() => {
+        if (inView) {
+            !isFetching && hasNextPage && fetchNextPage();
+        }
+    }, [inView, isFetching, hasNextPage, fetchNextPage])
 
     if (isError) {
         return <div>Error...</div>
     }
 
     return (
-        <div>
+        <div className='container mx-auto px-4 py-6'>
             <input value={search} onChange={(e) => setSearch(e.target.value)} />
-            {data?.data.data.map((lp) => <h1>{lp.title}</h1>)}
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+                {isPending && <LpCardSkeletonList count={20} />}
+                {lps?.pages
+                    ?.map((page) => page.data.data)
+                    ?.flat()
+                    ?.map((lp) => <LpCard key={lp.id} lp={lp} />)}
+                    {isFetching && <LpCardSkeletonList count={20} />}
+                <div ref={ref} className='h-2'></div>
+            </div>
         </div>
     )
 }

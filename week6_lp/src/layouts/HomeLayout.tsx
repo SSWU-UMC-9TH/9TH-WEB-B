b535b7e757599b9bd227b44909480d70e2f22120
nakyeong <yunna3425@naver.com>
2025-11-08 22:01:49 +0900

    Feat: Tanstck Query를 활용하여 LP 목록 조회

---
+++ week6_lp/src/layouts/HomeLayout.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext';
+import FloatingButton from '../components/FloatingButton';
+
+const HomeLayout = () => {
+    const navigate = useNavigate();
+    const { accessToken } = useAuth();
+
+    return (
+        <div className='h-dvh flex flex-col'>
+            <nav className='bg-[#161616] flex items-center justify-between h-[80px] p-[20px]'>
+                <button 
+                    className='text-[#ea00b1] font-bold text-[25px] cursor-pointer'
+                    onClick={() => navigate('/')}
+                >
+                    돌려돌려LP판
+                </button>
+                <div>
+                    {!accessToken ? (
+                        <>
+                            <button 
+                                className='text-white bg-black px-[10px] py-[5px] rounded-[5px] font-semibold cursor-pointer'
+                                onClick={() => navigate('/login')}
+                            >
+                                로그인
+                            </button>
+                            <button 
+                                className='text-white bg-[#ea00b1] px-[10px] py-[5px] ml-[10px] rounded-[5px] font-semibold cursor-pointer'
+                                onClick={() => navigate('/signup')}
+                            >
+                                회원가입
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <button 
+                                className='text-white bg-black px-[10px] py-[5px] rounded-[5px] font-semibold cursor-pointer'
+                                onClick={() => navigate('/my')}
+                            >
+                                마이페이지
+                            </button>
+                            <button 
+                                className='text-white bg-black px-[10px] py-[5px] rounded-[5px] font-semibold cursor-pointer ml-[10px]'
+                                onClick={() => navigate('/search')}
+                            >
+                                검색
+                            </button>
+                        </>
+                    )}
+                </div>
+            </nav>
+            <main className='flex-1 bg-black text-white'>
+                <Outlet />
+            </main>
+            <footer className='bg-black'>푸터</footer>
+            <FloatingButton />
+        </div>
+    )
+}
+
+export default HomeLayout
---
+++ week6_lp/src/pages/HomePage.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import useGetLpList from '../hooks/queries/useGetLpList'
+
+const HomePage = () => {
+    const [search, setSearch] = useState("");
+    const {data, isPending, isError} = useGetLpList({
+        search,
+    });
+
+
+    if (isPending) {
+        return <div>Loading...</div>
+    }
+
+    if (isError) {
+        return <div>Error...</div>
+    }
+
+    return (
+        <div>
+            <input value={search} onChange={(e) => setSearch(e.target.value)} />
+            {data?.data.data.map((lp) => <h1>{lp.title}</h1>)}
+        </div>
+    )
+}
+
+export default HomePage

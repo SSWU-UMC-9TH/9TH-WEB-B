c8ad2a8e5ee6606f6149fa27d3b983c7a04f2d21
nakyeong <yunna3425@naver.com>
2025-11-27 20:47:53 +0900

    Feat: useReducer 실습

---
+++ week9_useReducer/src/App.tsx
@@ -0,0 +1,12 @@
+import UseReducerPage from "./pages/UseReducerPage"
+import UseRedecerCompany from "./pages/UseRedecerCompany"
+
+function App() {
+    return (
+        <>
+            <UseRedecerCompany />
+        </>
+    )
+}
+
+export default App

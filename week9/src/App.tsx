d3f38e7e21e797d52396214097563bec8bf45619
riveryunny <gayunphone@gmail.com>
2025-12-01 03:32:51 +0900

    feat: CartSlice, ModalSlice - 미션 1, 2 완료

---
+++ week9/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import CartList from './components/CartList'; // CartList import 경로 수정
+import Navbar from './components/navbar';
+import React from 'react';
+import store from './store/store';
+import { Provider } from 'react-redux';
+import PriceBox from './components/PriceBox';
+import Modal from './components/Modal';
+
+function App(): React.ReactElement {
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartList />
+      <PriceBox />
+      <Modal />
+    </Provider>
+  );
+}
+
+export default App;

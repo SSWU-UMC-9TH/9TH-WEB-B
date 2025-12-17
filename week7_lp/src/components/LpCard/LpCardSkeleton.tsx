d3b1bd1be87d646cd8a3dd4720f9504cd145f63d
nakyeong <yunna3425@naver.com>
2025-12-18 08:20:10 +0900

    Revert "Merge pull request #68 from SSWU-UMC-9TH/#61/yuni-week7"
    This reverts commit a3aaf3aa3adcfea206c2231f88c9da03a30f80fa, reversing
    changes made to 7ad877c06972d6afdae48751000ec9802f90ee6a.

--- week7_lp/src/components/LpCard/LpCardSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-export const LpCardSkeleton = () => {
-    return (
-        <div className="w-48 h-48 animate-pulse">
-            <div className="w-full h-full bg-gray-700" />
-        </div>
-    )
-}
